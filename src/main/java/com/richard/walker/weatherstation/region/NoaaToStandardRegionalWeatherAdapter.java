e4a0df07cf03cc757542b4dcd5383c18c8a4bee6
Brandon Tweed <brandon.tweed@gmail.com>
2018-05-15 14:52:14 -0400

    Add the magic one-liner

--- src/main/java/com/richard/walker/weatherstation/region/NoaaToStandardRegionalWeatherAdapter.java
+++ src/main/java/com/richard/walker/weatherstation/region/NoaaToStandardRegionalWeatherAdapter.java
@@ -5,6 +5,7 @@ import com.richard.walker.weatherstation.domain.WeatherStation;
 import com.richard.walker.weatherstation.region.StandardRegionalWeather;
 import com.richard.walker.weatherstation.vendor.NoaaRegionalWeather;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NoaaToStandardRegionalWeatherAdapter implements StandardRegionalWeather {
@@ -21,10 +22,8 @@ public class NoaaToStandardRegionalWeatherAdapter implements StandardRegionalWea
 
     @Override
     public List<WeatherStation> getWeatherStations() {
-        // TODO: The standard API expects a List but the NOAA provides an Array. Convert the Array to a List.
-
         // The Arrays collection class already has an implementation:
         // https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html
-        return null;
+        return Arrays.asList(noaaRegionalWeather.getWeatherStations());
     }
 }
